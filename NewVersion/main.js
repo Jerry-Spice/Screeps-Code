11ae2cfc1c900b08fa01d0f9feb88ef0a9f5273e
Jerry Spice <36059502+Jerry-Spice@users.noreply.github.com>
2020-04-06 11:24:33 -0400

    Commented through the document
    added comments to main.js

--- NewVersion/main.js
+++ NewVersion/main.js
@@ -1,14 +1,19 @@
+//Importing Files for the creeps, spawns, and towers
 require("prototype.creep")();
 require("prototype.spawn")();
 require("prototype.tower")();
 
+//room swapper for the long harvesters
+// when a LH spawns it needs a target room and it gets it from this arrary.
 var targetRooms = ["E3N9"];
 var targetRoomSwapper = 0;
 
+//target flag swapper for defenders
+// when a defender spawns it looks for its target flag that it gets from this array.
 var targetFlags = ["P1", "P2"];
 var targetFlagSwapper = 0;
 
-
+//variables to track the number of each role
 var numberOfHarvesters;
 var numberOfMiners;
 var numberOfUpgraders;
@@ -18,6 +23,9 @@ var numberOfWallRepairers;
 var numberOfBuilders;
 var numberOfDefenders;
 
+//Old variables to track the numbers for how many of each role could exist.
+//The tracking system for that is now stored in the memory of the spawns.
+/*
 var maxHarvesters = 4;
 var maxMiners = 0;
 var maxUpgraders = 2;
@@ -26,31 +34,39 @@ var maxRepairers = 2;
 var maxWallRepairers = 1;
 var maxBuilders = 1;
 var minDefenders = 2;
-
+*/
 
 module.exports.loop = function () {
+    //for loop to keep the used memory down
+    // looks through the Creeps list in Memory
     for (let name in Memory.creeps) {
+        // if one of the creeps found doesnt exist anymore
         if (Game.creeps[name] == undefined) {
+            // if its because there are no creeps
             if (Game.creeps.length == 0) {
+                // spawn a harvester
                 Game.spawns["Spawn1"].spawnHarvester(300);
-            } else {
+            } else { // otherwise just remove that creep's data
                 delete Memory.creeps[name];
             }
         }
     }
-
+    // find all the structures and filter out only the towers
     var towers = _.filter(Game.structures, s => s.structureType == STRUCTURE_TOWER);
-    // for each tower
+    // for loop to run the defense code for each tower
     for (let i in towers) {
-        // run tower logic
+        // towers defend
         towers[i].defend();
     }
     
-
+    // for loop to loop through all the creeps that are alive
     for (let name in Game.creeps) {
+        // setting a creep variable for convenience
         var creep = Game.creeps[name];
-        var fatigueAmount = creep.fatigue;
+        //extra code to have the creeps say how fatigued they are
+        //var fatigueAmount = creep.fatigue;
         //creep.say(fatigueAmount);
+        //if the creep's role is harvester then do harvester work, if miner role then miner work etc.
         if (creep.memory.role == "harvester") {
             creep.HarvesterRole();
         }
@@ -82,11 +98,15 @@ module.exports.loop = function () {
             creep.ClaimerRole();
         }
     }
+    // for loop to loop through all the possible spawns
     for (let SpawnName in Game.spawns) {
+        // variables for convenience to have spawn as the current spawn
+        // maxEnergy as the total amount of energy the spawn is able to use
         var spawn = Game.spawns[SpawnName];
         var maxEnergy = Game.spawns[SpawnName].room.energyAvailable;
 
-
+        // trackers to find how many of each role is currently alive
+        // there are some bugs, for example the repairers will not track accurately if the && is in with it
         numberOfHarvesters = _.sum(Game.creeps, (c) => c.memory.role == "harvester" && c.room.name == spawn.room.name);
         numberOfMiners = _.sum(Game.creeps, (c) => c.memory.role == "miner" && c.room.name == spawn.room.name);
         numberOfUpgraders = _.sum(Game.creeps, (c) => c.memory.role == "upgrader" && c.room.name == spawn.room.name);
@@ -96,6 +116,17 @@ module.exports.loop = function () {
         numberOfBuilders = _.sum(Game.creeps, (c) => c.memory.role == "builder" && c.room.name == spawn.room.name);
         numberOfDefenders = _.sum(Game.creeps, (c) => c.memory.role == "defender" && c.room.name == spawn.room.name);
         
+        //logic to determine priority and spawn the necesary creeps.
+        // priority:
+        /* 1. Harvester
+           2. Miner
+           3. Upgrader
+           4. Long Harvester
+           5. Repairer
+           6. Wall Repairer
+           7. Builder
+           8. Defender
+        */
         if (numberOfHarvesters < spawn.memory.maxHarvesters) {
             spawn.spawnHarvester(maxEnergy);
         } else if (numberOfMiners < spawn.memory.maxMiners) {
